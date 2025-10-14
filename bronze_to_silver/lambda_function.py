764c2db5008898f0a80baffaecd9752515ca254d
BryanGuapulema <bryan19012003@gmail.com>
2025-10-14 16:33:37 -0500

    log status for this lambda

--- bronze_to_silver/lambda_function.py
+++ bronze_to_silver/lambda_function.py
@@ -3,8 +3,12 @@ from etl_dim_store import run_dim_store
 from etl_dim_products import run_dim_products
 from etl_dim_customers import run_dim_customers
 from etl_dim_employees import run_dim_employees
+import json, logging
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
 
 def handler(event, context):
+    log.info(f"EVENT: {json.dumps(event)}")
     run_month = event.get("run_month")  # "YYYY-MM"
     if not run_month:
         raise RuntimeError("Falta run_month (YYYY-MM)")
@@ -20,7 +24,9 @@ def handler(event, context):
         run_dim_customers(run_month)
         run_dim_employees(run_month)
 
-    return {"status": "SUCCEEDED", "run_month": run_month, "refresh_dims": refresh_dims}
+    result = {"status": "SUCCEEDED", "run_month": run_month, "refresh_dims": refresh_dims}
+    log.info(f"RESULT: {json.dumps(result)}")
+    return result
 
 def lambda_handler(event, context):
     return handler(event, context)
