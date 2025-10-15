a6e0a74ec68b9f85b39c3abda9047e3ba555b3a1
BryanGuapulema <bryan19012003@gmail.com>
2025-10-15 11:33:57 -0500

    insert fact first month test

---
+++ athena_etl_warehouse/4_insert_fact_first_month/fact_sales.sql
@@ -0,0 +1,27 @@
+INSERT INTO hack2_aw_catalog.fact_sales
+SELECT
+  o.SalesOrderID            AS SalesOrderKey,
+  o.SalesOrderDetailID      AS SalesOrderDetailKey,
+
+  -- Fechas: ajusta el patrón si tu CSV tiene otro formato
+  TRY(date_parse(o.OrderDate, '%m/%e/%Y')) AS OrderDate,
+  TRY(date_parse(o.DueDate,   '%m/%e/%Y')) AS DueDate,
+  TRY(date_parse(o.ShipDate,  '%m/%e/%Y')) AS ShipDate,
+
+  o.EmployeeID              AS EmployeeKey,
+  o.CustomerID              AS CustomerKey,
+  o.ProductID               AS ProductKey,
+  o.StoreID                 AS StoreKey,
+
+  o.OrderQty,
+  o.UnitPrice,
+  o.UnitPriceDiscount,
+  o.SubTotal,
+  o.TaxAmt,
+  o.Freight,
+  o.TotalDue,
+  o.LineTotal,
+
+  '2011-05'                 AS RunMonth
+FROM hack2_aw_catalog.ext_orders o
+WHERE "$path" = 's3://bg-hack2-aw-datalake2/bronze/source=github/table=orders/orders_2011-05.csv';
